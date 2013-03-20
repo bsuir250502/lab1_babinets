0a03d1be9b779bff6f79e2f7aef1e4b45ceda604
Yuri Babinets <admin@cosmostv.tk>
2013-03-20 21:31:12 +0300

    Added main.c

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct inf
+{
+    char f_name[20], name[15], patr[15];
+    int sem;
+}
+
+int main()
+{
+    printf("Source comming soon...");
+    return 0;
+}
