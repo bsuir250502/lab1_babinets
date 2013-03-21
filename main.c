e6de8d66b4387e706d845918246a5d1080ec08da
Yuri Babinets <admin@cosmostv.tk>
2013-03-21 09:03:26 +0300

    Starting... 4%

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct inf
+struct stud_name
 {
     char f_name[20], name[15], patr[15];
     int sem;
@@ -9,6 +9,25 @@ struct inf
 
 int main()
 {
-    printf("Source comming soon...");
+    int i;
+    stud_name stud[5];
+    stud[0].f_name="Ivanov";
+    stud[1].f_name="Petrov";
+    stud[2].f_name="Sidorov";
+    stud[3].f_name="Fam3";
+    stud[4].f_name="Fam4";
+	
+    for(i=0; i<5; i++)
+    {
+        puts("Enter %d student's first name", i);
+        gets(stud[i].f_name);
+        puts("Enter %d student's name", i);
+        gets(stud[i].name);
+        puts("Enter %d student's", i);
+        gets(stud[i].patr);
+        puts("Enter %d semestr", i);
+        scanf_s("%d", &stud[i].sem);
+    }
+    puts("Comming soon...");
     return 0;
 }
