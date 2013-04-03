fa89856c54abc9aaabb33f0432b9432df9ce909e
Yuri Babinets <admin@cosmostv.tk>
2013-04-03 11:21:06 +0300

    Added input_names function, safe input of semesters. Working on source...

--- main.c
+++ main.c
@@ -31,20 +31,29 @@ void ins_stud_names();
 int main(char *argv[])
 {
 	char name_of_exams[][]= {"Math, Arithmetics, Programming",
-							"Math, Programming, Physics, English"};
+							"Math, Programming, Physics, English"}, tmp;
     int i, sem;
 
     
     for(i=0; i<n; i++)
     {
-        printf("Enter %d student's first name", i);
+        /*printf("Enter %d student's first name", i);
         fgets(stud[i].name.f_name, 20, stdin);
         printf("Enter %d student's name", i);
         fgets(stud[i].name.name, 15, stdin);
         printf("Enter %d student's", i);
-        fgets(stud[i].name.patr, 15, stdin);
-        printf("Enter %d student semestr", i);
-        fgets("%d", &stud[i].name.sem);
+        fgets(stud[i].name.patr, 15, stdin);*/
+		printf("Enter %d student semestr", i);
+		while(1)
+		{
+			fgets(tmp, 1, stdin);
+			if(atoi(tmp)<1 || atoi(tmp)>2) puts("Enter correct semestr number!");
+			else 
+			{
+				stud[i].name.sem=atoi(tmp);
+				break;
+			}
+		}
     }
     puts("Enter semestr");
     scanf_s("%d", &sem);
@@ -59,19 +68,19 @@ int main(char *argv[])
 
 void ins_stud_names()
 {
-	marks[0].student.surname
-	marks[0].student.name
-	marks[0].student.patr
-	marks[1].student.surname
-	marks[1].student.name
-	marks[1].student.patr
-	marks[2].student.surname
-	marks[2].student.name
-	marks[2].student.patr
-	marks[3].student.surname
-	marks[3].student.name
-	marks[3].student.patr
-	marks[4].student.surname
-	marks[4].student.name
-	marks[4].student.patr
+	marks[0].student.surname="Ivanov";
+	marks[0].student.name="Ivan";
+	marks[0].student.patr="Ivanovich";
+	marks[1].student.surname="Petrov";
+	marks[1].student.name="Petr";
+	marks[1].student.patr="Petrovich";
+	marks[2].student.surname="Sidorov";
+	marks[2].student.name="Ivan";
+	marks[2].student.patr="Pavlovich";
+	marks[3].student.surname="Mojako";
+	marks[3].student.name="Anton";
+	marks[3].student.patr="Vladimirovich";
+	marks[4].student.surname="Sheleg";
+	marks[4].student.name="Nikolay";
+	marks[4].student.patr="Borisovich";
 }
