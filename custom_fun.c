7bebfb653c6489ab5586b7674633bb7d3f2a38d2
Yuri Babinets <admin@cosmostv.tk>
2013-05-07 16:01:51 +0300

    Working on lab...

--- custom_fun.c
+++ custom_fun.c
@@ -1,22 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int scan_int(char *message, int buffer, int min, int max)
+
+int scan_int(char *message, int min, int max)
 {
-    char input[buffer];
+    char input[1024];
 	int num;
     printf("%s ", message);
     while(1)
     {
-        fgets(input, buffer, stdin);
+        fgets(input, 1024, stdin);
         if(num=atoi(input)<min || num>max) puts("Enter correct number! Retry...");
         else return num;
     }
-    return -1;
+    return 0;
 }
 
-char scan_char(char *message, int buffer)
+/*char scan_char(char *message, int buffer)
 {
     char input[buffer];
     printf("%s ", message);
     return fgets(input, buffer, stdin);
-}
+}*/
